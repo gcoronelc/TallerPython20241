a893b5c4a3419092f183c99a7c16939135471fed
Eric Gustavo Coronel Castillo <gcoronel@vallegrande.edu.pe>
2024-04-21 09:05:12 -0500

    Semana 7

--- Semana07/prog02.py
+++ Semana07/prog02.py
@@ -1,4 +1,56 @@
-import modulo1
-print(modulo1.suma(10,7))
-print(modulo1.resta(10,7))
-print(modulo1.multiplica(10,7))
+# import modulo1
+# print(modulo1.suma(10,7))
+# print(modulo1.resta(10,7))
+# print(modulo1.multiplica(10,7))
+
+
+# import modulo1 as mo
+# print(mo.suma(10,7))
+# print(mo.resta(10,7))
+# print(mo.multiplica(10,7))
+
+
+# from modulo1 import suma
+# print(suma(10,7))
+
+
+# from modulo1 import suma as s, resta
+# print(s(10,7))
+# print(resta(10,7))
+
+
+
+
+# from modulo1 import *
+
+# def suma(num1,num2):
+#     return num1 + num2*2
+
+# print(suma(10,7))
+# print(resta(10,7))
+# print(multiplica(10,7))
+
+
+
+
+
+# import modulo1 as m
+
+# def suma(num1,num2):
+#     return num1 + num2*2
+
+# print(suma(10,7))
+# print(m.suma(10,7))
+# print(m.resta(10,7))
+# print(m.multiplica(10,7))
+
+
+
+from modulo1 import suma as s, resta
+
+def suma(num1,num2):
+    return num1 + num2*2
+
+print(suma(10,7))
+print(s(10,7))
+print(resta(10,7))
